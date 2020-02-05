feeccc647e75ac4f84718e796c32f733fa59915f
Richa Deshwal <richa.deshwal@loblaw.ca>
2020-02-05 11:30:59 -0500

    header footer and parallax effect

---
+++ src/MainContent/MainContent.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./MainContent.css";
+
+function MainContent() {
+  return (
+    <div>
+      <div class="pimg1">
+        <div class="ptext">
+          <strong class="border cover-border">Richa Deshwal</strong>
+        </div>
+      </div>
+      <section class="section section-light">
+        <h2>ABOUT ME</h2>
+
+        <p>
+          With over 9 years of professional experience in technology consulting,
+          working as Front End Engineer, Technical Lead, and Scrum Master, I
+          have delivered over 25 web projects with clients from insurance,
+          pharmaceutical, telecommunications, and manufacturing industry based
+          in U.S. and Canada. Beyond the skill and experience, I bring in the
+          passion to deliver beyond my responsibilities. I believe in learning
+          each day, to be better the next day. Please go through my website to
+          find out more.
+        </p>
+      </section>
+      <div class="pimg2">
+        <div class="ptext">
+          <span class="border trans">Image two</span>
+        </div>
+      </div>
+      <section class="section section-dark">
+        <h2>Section two</h2>
+        <p>
+          Other some text in section two. Other some text in section two. Other
+          some text in section two. Other some text in section two. Other some
+          text in section two. Other some text in section two. Other some text
+          in section two.
+        </p>
+      </section>
+      <div class="pimg3">
+        <div class="ptext">
+          <strong class="border trans">
+            SENIOR CONSULTANT AT DELOITTE CANADA
+          </strong>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainContent;
